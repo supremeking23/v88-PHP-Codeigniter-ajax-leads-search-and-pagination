1949316750e0b819abc4ec3e1f2d7aeec40daa7c
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-04-30 08:21:07 +0800

    add date filter

--- application/views/leads/includes/form.php
+++ application/views/leads/includes/form.php
@@ -22,15 +22,13 @@
 
 
         <div class="row d-flex justify-content-between">
-            
-                <div class="col-md-3">
-                    <form action="leads/search_by_name" method="POST" class="search_by_name">
-                        <div class="form-group">
-                            <input type="search" name="search" class="form-control" id="search" placeholder="search here...">
-                        </div>
-                    </form>
-                </div>
-            
+            <div class="col-md-3">
+                <form action="leads/search_by_name" method="POST" class="search_by_name">
+                    <div class="form-group">
+                        <input type="search" name="search" class="form-control" id="search" placeholder="search here...">
+                    </div>
+                </form>
+            </div>
             <div class="col-md-5">
                 <form action="leads/search_by_date" method="POST" class="search_date d-flex justify-content-between align-items-start">
                     <div class="form-group">
@@ -39,7 +37,7 @@
                     <div class="form-group">
                         <input type="date" name="to" class="form-control" id="to" >
                     </div>
-                    <button type="submit" class="btn  btn-primary"><i class="fas fa-search"></i></button>
+                   
                 </form>
             </div>
         </div>
--- application/views/leads/index.php
+++ application/views/leads/index.php
@@ -9,6 +9,160 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     <title>Leads search and pagination</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <script>
+        $(document).ready(function(){
+
+            function load_leads(res){
+                let html = ``;
+                    for(let i = 0; i < res.leads.length; i++){
+                        // console.log(res.leads[i]);
+                        html += `<tr>`;
+                        html += `   <th scope="row">${res.leads[i].leads_id}</th>`;
+                        html += `   <td>${res.leads[i].first_name}</td>`;
+                        html += `   <td>${res.leads[i].last_name}</td>`;
+                        html += `   <td>${res.leads[i].date_joined}</td>`;
+                        html += `   <td>${res.leads[i].email}</td>`;
+                        html += `</tr>`;
+                    }
+                return html;
+            }
+
+            $('.lead_datas').html(`<tr><td colspan="5" style="text-align:center"><img src='<?= base_url()?>assets/img/loading.gif'></td></tr>`);
+            $.get("leads/index_json",function(res){
+                let html = load_leads(res);
+                $('table').addClass("table-striped");
+                $(".lead_datas").html(html);
+            },'json');
+
+
+            $.get("leads/pagination_count",function(res){
+                console.log(res);
+                console.log(res.count_leads[0].page_count);
+                let html = ``;
+                for(let i = 0; i < res.count_leads[0].page_count/50; i++){
+                //     // console.log(res.leads[i]);
+                html += `<li class="page_item">`;
+                html += `   <a class="page-link" href="${i}">${i}</a>`;
+                html += `</li>`;
+                }
+                $(".pagination").html(html);
+            },'json');
+
+            $('#search').on('input',function(event){
+               
+                let search_form = $(this).parent().parent();
+                let serialize_search_form = search_form.serialize();
+
+                $('table').removeClass("table-striped");
+                $('.lead_datas').html(`<tr><td colspan="5" style="text-align:center"><img src='<?= base_url()?>assets/img/loading.gif'></td></tr>`);
+                $.post(search_form.attr('action'), serialize_search_form, function(res) {
+                    let html = load_leads(res);
+                    $('table').addClass("table-striped");
+                    $(".lead_datas").html(html);
+                },'json');
+            });
+
+            $("form").submit(function(){
+               
+                return false;
+            });
+
+            
+
+            // $("form").submit(function(){
+            //     console.log($(this).serialize());
+            //     $.post($(this).attr('action'), $(this).serialize(), function(res) {
+            //         let html = ``;
+            //         for(let i = 0; i < res.leads.length; i++){
+            //             // console.log(res.leads[i]);
+            //             html += `<tr>`;
+            //             html += `   <th scope="row">${res.leads[i].leads_id}</th>`;
+            //             html += `   <td>${res.leads[i].first_name}</td>`;
+            //             html += `   <td>${res.leads[i].last_name}</td>`;
+            //             html += `   <td>${res.leads[i].date_joined}</td>`;
+            //             html += `   <td>${res.leads[i].email}</td>`;
+            //             html += `</tr>`;
+            //         }
+            //         $(".lead_datas").html(html);
+            //         console.log(res);
+                    
+            //     },'json');
+                
+            //     console.log(`console`);
+            //     console.log('sd',$(this).attr('action'));
+
+            //     return false;
+            // });
+
+
+            // $("#from").on('input',function(event){
+            //     console.log("from");
+            //     let date_form = $(this).parent().parent();
+            //     let serialize_date_form = date_form.serialize();
+            //     console.log(serialize_date_form);
+
+            //     $('table').removeClass("table-striped");
+            //     $('.lead_datas').html(`<tr><td colspan="5" style="text-align:center"><img src='<?= base_url()?>assets/img/loading.gif'></td></tr>`);
+            //     $.post(date_form.attr('action'), serialize_date_form, function(res) {
+            //         let html = load_leads(res);
+            //         $('table').addClass("table-striped");
+            //         $(".lead_datas").html(html);
+            //     },'json');
+                
+            // });
+
+            $("#to").on('input',function(event){
+                // let name = $("#search").val();
+                // let from = $("#from").val();
+                // let to = $("#to").val();
+                // alert(from);
+                console.log("to");
+                let date_form = $(this).parent().parent();
+                let serialize_date_form = date_form.serialize();
+                console.log(serialize_date_form);
+
+                $('table').removeClass("table-striped");
+                $('.lead_datas').html(`<tr><td colspan="5" style="text-align:center"><img src='<?= base_url()?>assets/img/loading.gif'></td></tr>`);
+                $.post(date_form.attr('action'), serialize_date_form, function(res) {
+                    let html = load_leads(res);
+                    $('table').addClass("table-striped");
+                    $(".lead_datas").html(html);
+                },'json');
+                
+            });
+
+            // $(".search_date").submit(function(){
+                
+               
+            //     $.post(`leads/search_by_date`, $(".search_date").serialize(), function(res) {
+            //         // alert("dotp");
+            //         // alert(from);
+            //         console.log(res);
+            //         let html = ``;
+            //         for(let i = 0; i < res.leads.length; i++){
+            //             // console.log(res.leads[i]);
+            //             html += `<tr>`;
+            //             html += `   <th scope="row">${res.leads[i].leads_id}</th>`;
+            //             html += `   <td>${res.leads[i].first_name}</td>`;
+            //             html += `   <td>${res.leads[i].last_name}</td>`;
+            //             html += `   <td>${res.leads[i].date_joined}</td>`;
+            //             html += `   <td>${res.leads[i].email}</td>`;
+            //             html += `</tr>`;
+            //         }
+            //         $(".lead_datas").html(html);
+            //     },'json');
+            // });
+                
+                
+           
+
+
+        });
+    </script>
   </head>
   <body>
     <!-- As a heading -->
@@ -62,107 +216,6 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-
-    <script>
-        $(document).ready(function(){
-            $('.lead_datas').html(`<tr><td colspan="5" style="text-align:center"><img src='<?= base_url()?>assets/img/loading.gif'></td></tr>`);
-            $.get("leads/index_json",function(res){
-                let html = ``;
-                for(let i = 0; i < res.leads.length; i++){
-                    // console.log(res.leads[i]);
-                    html += `<tr>`;
-                    html += `   <th scope="row">${res.leads[i].leads_id}</th>`;
-                    html += `   <td>${res.leads[i].first_name}</td>`;
-                    html += `   <td>${res.leads[i].last_name}</td>`;
-                    html += `   <td>${res.leads[i].date_joined}</td>`;
-                    html += `   <td>${res.leads[i].email}</td>`;
-                    html += `</tr>`;
-                }
-                $('table').addClass("table-striped");
-                $(".lead_datas").html(html);
-            },'json');
-
-
-            $.get("leads/pagination_count",function(res){
-                console.log(res);
-                console.log(res.count_leads[0].page_count);
-                let html = ``;
-                for(let i = 0; i < res.count_leads[0].page_count/50; i++){
-                //     // console.log(res.leads[i]);
-                html += `<li class="page_item">`;
-                html += `   <a class="page-link" href="${i}">${i}</a>`;
-                html += `</li>`;
-                }
-                $(".pagination").html(html);
-            },'json');
-
-
-
-           
-
-            $("form").submit(function(){
-                console.log($(this).serialize());
-                $.post($(this).attr('action'), $(this).serialize(), function(res) {
-                    let html = ``;
-                    for(let i = 0; i < res.leads.length; i++){
-                        // console.log(res.leads[i]);
-                        html += `<tr>`;
-                        html += `   <th scope="row">${res.leads[i].leads_id}</th>`;
-                        html += `   <td>${res.leads[i].first_name}</td>`;
-                        html += `   <td>${res.leads[i].last_name}</td>`;
-                        html += `   <td>${res.leads[i].date_joined}</td>`;
-                        html += `   <td>${res.leads[i].email}</td>`;
-                        html += `</tr>`;
-                    }
-                    $(".lead_datas").html(html);
-                    console.log(res);
-                    
-                },'json');
-                
-                console.log(`console`);
-                console.log('sd',$(this).attr('action'));
-
-                return false;
-            });
-
-
-            $("#from").change(function(){
-                // let name = $("#search").val();
-                // let from = $("#from").val();
-                // let to = $("#to").val();
-                // alert(from);
-                
-            });
-
-            // $(".search_date").submit(function(){
-                
-               
-            //     $.post(`leads/search_by_date`, $(".search_date").serialize(), function(res) {
-            //         // alert("dotp");
-            //         // alert(from);
-            //         console.log(res);
-            //         let html = ``;
-            //         for(let i = 0; i < res.leads.length; i++){
-            //             // console.log(res.leads[i]);
-            //             html += `<tr>`;
-            //             html += `   <th scope="row">${res.leads[i].leads_id}</th>`;
-            //             html += `   <td>${res.leads[i].first_name}</td>`;
-            //             html += `   <td>${res.leads[i].last_name}</td>`;
-            //             html += `   <td>${res.leads[i].date_joined}</td>`;
-            //             html += `   <td>${res.leads[i].email}</td>`;
-            //             html += `</tr>`;
-            //         }
-            //         $(".lead_datas").html(html);
-            //     },'json');
-            // });
-                
-                
-           
-
-
-        });
-    </script>
+ 
   </body>
 </html>
